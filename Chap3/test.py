e14a41993d7ff2c6f1d77a8574e2aacedf0d5896
Subs_Way <quhb2455@gmail.com>
2022-06-09 17:05:39 +0900

    -

--- Chap3/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-a = np.ones([16,28,28,1])
-b = np.zeros([1,28,28,1])
-c = np.concatenate([a, b])
-print(c.shape)
--- Chap4/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from random import randint
-
-a = np.load('./church_outdoor_train_lmdb_gray.npy')
-
-print(a.shape)
